24e5848fb1c90419f2bab4da5b4e40da5e870a58
KeitaKobayashi-a <kobayashi-keita@jreast.co.jp>
2025-12-08 17:09:40 +0900

    feat: add data models

---
+++ backend/src/models.rs
@@ -0,0 +1,31 @@
+use serde::{Serialize, Deserialize};
+use uuid::Uuid;
+use chrono::{DateTime, Utc, FixedOffset, TimeZone};
+use sqlx::FromRow;
+
+#[derive(Debug, Clone, Serialize, Deserialize, FromRow)]
+pub struct Todo{
+    pub id: Uuid,
+    pub title: String,
+    pub description: Option<String>,
+    pub completed: bool,
+    pub created_at: DateTime<Utc>,
+    pub updated_at: DateTime<Utc>,
+    }
+
+impl Todo{
+    pub fn new(title: String, description: String) -> Self{
+        let jst = FixedOffset::east_opt(9 * 3600).unwrap();
+                let now_jst = jst.from_utc_datetime(&Utc::now().naive_utc());
+                let now_utc = now_jst.with_timezone(&Utc);
+
+                Self {
+                    id: Uuid::now_v7(),
+                    title,
+                    description: Some(description),
+                    completed: false,
+                    created_at: now_utc,
+                    updated_at: now_utc
+                    }
+        }
+    }
