05a320e33fe48c5c5f5195be7520507e8672b69b
KeitaKobayashi-a <kobayashi-keita@jreast.co.jp>
2025-12-08 13:44:28 +0900

    chore: add uuid

---
+++ backend/target/debug/deps/backend-fe6e90ac46950e9d.d
@@ -0,0 +1,5 @@
+/Users/kobayashi/Documents/BTC/presentation/rust-todo/backend/target/debug/deps/backend-fe6e90ac46950e9d.d: src/main.rs
+
+/Users/kobayashi/Documents/BTC/presentation/rust-todo/backend/target/debug/deps/backend-fe6e90ac46950e9d: src/main.rs
+
+src/main.rs:
