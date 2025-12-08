ce4d59634f65160f90de1dfc9d2f3e4bc24b3cd6
KeitaKobayashi-a <kobayashi-keita@jreast.co.jp>
2025-12-08 14:46:26 +0900

    chore: add sqlxライブラリ

---
+++ backend/target/debug/deps/backend-b9f8fb927c684b0d.d
@@ -0,0 +1,7 @@
+/Users/kobayashi/Documents/BTC/presentation/rust-todo/backend/target/debug/deps/backend-b9f8fb927c684b0d.d: src/main.rs
+
+/Users/kobayashi/Documents/BTC/presentation/rust-todo/backend/target/debug/deps/backend-b9f8fb927c684b0d: src/main.rs
+
+src/main.rs:
+
+# env-dep:CARGO_CRATE_NAME=backend
