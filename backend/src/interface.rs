508ca2488984bf4dace6fddbc485fb2ab4084547
KeitaKobayashi-a <kobayashi-keita@jreast.co.jp>
2025-12-08 18:00:34 +0900

    feat: add interface

---
+++ backend/src/interface.rs
@@ -0,0 +1,12 @@
+use crate::models::Todo;
+use uuid::Uuid;
+use async_trait::async_trait;
+
+#[async_trait]
+pub trait ToDoRepository{
+    async fn find_all(&self) -> Result<Vec<Todo> ,sqlx::Error>;
+    async fn find_by_id(&self, id: Uuid) -> Result<Option<Todo>, sqlx::Error>;
+    async fn create(&self, todo: Todo) -> Result<Todo, sqlx::Error>;
+    async fn update(&self, todo: Todo) -> Result<Todo, sqlx::Error>;
+    async fn delete(&self, id: Uuid) -> Result<(), sqlx::Error>;
+    }
