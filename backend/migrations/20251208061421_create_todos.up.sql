0aca1ec303b2f1f6765c4267ca058c5c6684bfa6
KeitaKobayashi-a <kobayashi-keita@jreast.co.jp>
2025-12-09 22:42:53 +0900

    feat: add seed data

--- backend/migrations/20251208061421_create_todos.up.sql
+++ backend/migrations/20251208061421_create_todos.up.sql
@@ -7,3 +7,13 @@ CREATE TABLE todos (
     created_at TIMESTAMP WITH TIME ZONE DEFAULT (now() AT TIME ZONE 'Asia/Tokyo') NOT NULL,
     updated_at TIMESTAMP WITH TIME ZONE DEFAULT (now() AT TIME ZONE 'Asia/Tokyo') NOT NULL
 );
+
+INSERT INTO todos(title, description) VALUES(
+    'wake up 6 AM', 'this is sample text'
+),(
+    'eat breakfast', 'this is sample text'
+),(
+    'get ready to go out', 'this is sample text'
+),(
+    'get home', 'this is sample text'
+)
