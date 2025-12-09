212ab5387ec64205ed54d41ee6271e572db26493
KeitaKobayashi-a <kobayashi-keita@jreast.co.jp>
2025-12-09 13:24:06 +0900

    feat: add service layer

---
+++ backend/src/service.rs
@@ -0,0 +1,20 @@
+use crate::models::Todo;
+use crate::interface::TodoRepository;
+use async_trait::async_trait;
+use uuid:: Uuid;
+
+#[derive(Clone)]
+pub struct TodoService<T: TodoRepository + Clone> {
+    repository: T,
+    }
+
+impl<T: TodoRepository + Clone> TodoService<T>{
+    pub fn new(repository: T) -> Self{
+        Self{repository}}
+    }
+
+#[async_trait]
+pub trait TodoService{
+    async fn get_all_todos(&self) -> Result<Vec<Todo>, sqlx::Error>;
+    async fn get_todo_by_id(&self, id: Uuid) -> Result<Option<Todo>, sqlx::Error>;
+    }
